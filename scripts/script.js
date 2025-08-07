e5738c56c2031197aff65b0f4e4bdb04473baee0
Laura Gong <lg443@cornell.edu>
2025-08-07 17:11:21 -0400

    balloons!

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,45 @@ let isGameOver = false;
 let walkFrame = 0;
 let walkInterval;
 
+function createBalloon() {
+  const balloon = document.createElement("img");
+  balloon.classList.add("balloon");
+
+  const balloonImages = [
+    "balloon-blue.png",
+    "balloon-green.png",
+    "balloon-yellow.png",
+    "balloon-pink.png"
+  ];
+  const randomImage = balloonImages[Math.floor(Math.random() * balloonImages.length)];
+  balloon.src = "images/" + randomImage;
+
+  const startLeft = Math.random() * game.offsetWidth;
+  balloon.style.left = `${startLeft}px`;
+
+  const scale = 0.5 + Math.random() * 0.5;
+  balloon.style.transform = `scale(${scale})`;
+  balloon.style.setProperty("--scale", scale);
+
+
+  game.appendChild(balloon);
+
+  let bottom = -200;
+  const floatSpeed = 1 + Math.random() * 1.5;
+  const floatInterval = setInterval(() => {
+    if (bottom > game.offsetHeight + 100) {
+      clearInterval(floatInterval);
+      balloon.remove();
+    } else {
+      bottom += floatSpeed;
+      balloon.style.bottom = `${bottom}px`;
+    }
+  }, 15);
+
+  const nextBalloonDelay = Math.random() * 2000 + 1000;
+  setTimeout(createBalloon, nextBalloonDelay);
+}
+
 function startWalkAnimation() {
   walkInterval = setInterval(() => {
     if (!isJumping && !isGameOver) {
@@ -143,3 +182,5 @@ document.addEventListener("keydown", (e) => {
 
 createObstacle();
 startWalkAnimation();
+createBalloon();
+createBalloon();
